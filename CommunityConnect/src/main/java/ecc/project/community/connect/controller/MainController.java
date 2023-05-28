44b35970e2a6352413e2e37e5f558cbe266ffd69
mrunalpatil1198 <56335027+mrunalpatil1198@users.noreply.github.com>
2023-05-28 13:47:36 -0400

    Add files via upload

---
+++ CommunityConnect/src/main/java/ecc/project/community/connect/controller/MainController.java
@@ -0,0 +1,32 @@
+package ecc.project.community.connect.controller;
+
+import ecc.project.community.connect.service.ErrorLoggingService;
+import lombok.AllArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Validated
+@AllArgsConstructor
+public class MainController {
+
+    private final ErrorLoggingService errorLoggingService;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Indiana University! ";
+    }
+
+
+    @GetMapping("/name")
+    public String getName(){
+        return "CommunityConnect";
+    }
+
+    @GetMapping("/error")
+    public void throwError(){
+        errorLoggingService.throwError();
+    }
+}
